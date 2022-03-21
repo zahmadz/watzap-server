f53c5d3d8907df657c3f8500e4ffd48d504baa3b
zulfikar <zulfikar1ahmad@gmail.com>
2022-03-21 10:18:38 +0700

    enable cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function initApp() {
     // security, cookie parser & body parser
     app.use(helmet());
     app.use(
-      cors({ origin: process.env.CLIENTS_URL.split(' '), credentials: true })
+      cors({ origin: process.env.CLIENTS_URL.split(','), credentials: true })
     );
     app.use(cookieParser());
     app.use(express.json());
